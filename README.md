cf2f4d19a586268d9be262f28cc4e01ffd661312
Michael H <107985466+mikedev23@users.noreply.github.com>
2022-07-21 16:39:44 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Michael's Emoji
+
+This is a modified emoji project from Scrimba that describes my personality thru emojis. Buttons are added to delete or add emojis to the front of my favorites or the least of my favorite interest to the back.
+
+## 🚀 About Me
+
+I'm an aspiring front-end developer taking courses on Scrimba with the goal of completion and certification.
+
+## 🛠️ Skills
+
+Javascript, HTML, CSS...
---
+++ app.js
@@ -0,0 +1,42 @@
+const myEmojis = ["👨🏿‍💻", "🍳", "🏀", "🏃🏿‍♂️", "☕"]
+const emojiContainer = document.querySelector("#emojiContainer")
+const emojiInput = document.querySelector("#emoji-input")
+const pushBtn = document.querySelector("#push-btn")
+const unshiftBtn = document.querySelector("#unshift-btn")
+const popBtn = document.querySelector("#pop-btn")
+const shiftBtn = document.querySelector("#shift-btn")
+
+renderEmojis()
+
+function renderEmojis() {
+	emojiContainer.innerHTML = ""
+	for (const emojiIndex of myEmojis) {
+		emojiContainer.innerHTML += `<span>${emojiIndex}</span>`
+	}
+}
+
+pushBtn.addEventListener("click", () => {
+	if (emojiInput.value) {
+		myEmojis.push(emojiInput.value)
+		emojiInput.value = ""
+		renderEmojis()
+	}
+})
+
+unshiftBtn.addEventListener("click", () => {
+	if (emojiInput.value) {
+		myEmojis.unshift(emojiInput.value)
+		emojiInput.value = ""
+		renderEmojis()
+	}
+})
+
+popBtn.addEventListener("click", () => {
+	myEmojis.pop()
+	renderEmojis()
+})
+
+shiftBtn.addEventListener("click", () => {
+	myEmojis.shift()
+	renderEmojis()
+})
